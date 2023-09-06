a12ad7d2a522b314b265878f21660b9e2a18b018
Victor Gorka De Prada Arnedo <vde-prad@student.42malaga.com>
2023-09-06 20:01:49 +0200

    request class created; setters and getters implemented

---
+++ Request.cpp
@@ -0,0 +1,43 @@
+#include "Request.hpp"
+
+// Constructors
+Request::Request()
+{
+	std::cout << "\e[0;33mDefault Constructor called of Request\e[0m" << std::endl;
+}
+
+Request::Request(const Request &copy)
+{
+	*this = copy;
+	std::cout << "\e[0;33mCopy Constructor called of Request\e[0m" << std::endl;
+}
+
+
+// Destructor
+Request::~Request()
+{
+	std::cout << "\e[0;31mDestructor called of Request\e[0m" << std::endl;
+}
+
+// Methods
+void		Request::setRawRequest(std::string raw)		{_rawRequest = raw;}
+void		Request::setMethod(std::string method)		{_method = method;}
+void		Request::setPath(std::string path)			{_path = path;}
+void		Request::setProtocol(std::string protocol)	{_protocol = protocol;}
+void		Request::setHost(std::string host)			{_host = host;}
+
+std::string	Request::getRawRequest(void)				{return _rawRequest;}
+std::string	Request::getMethod(void)					{return _method;}
+std::string	Request::getPath(void)						{return _path;}
+std::string	Request::getProtocol(void)					{return _protocol;}
+std::string	Request::getHost(void)						{return _host;}
+
+// Operators
+Request & Request::operator=(const Request &assign)
+{
+	this->setRawRequest(assign._rawRequest);
+	this->setMethod(assign._method);
+	this->setProtocol(assign._protocol);
+	return *this;
+}
+
---
+++ Request.hpp
@@ -0,0 +1,45 @@
+#ifndef REQUEST_HPP
+# define REQUEST_HPP
+
+# include <iostream>
+# include <string>
+
+class Request
+{
+	public:
+		// Constructors
+		Request();
+		Request(const Request &copy);
+		
+		// Destructor
+		~Request();
+		
+		// Operators
+		Request & operator=(const Request &assign);
+
+		// Methods //
+		
+
+		// Setters
+		void		setRawRequest(std::string);
+		void		setMethod(std::string);
+		void		setPath(std::string);
+		void		setProtocol(std::string);
+		void		setHost(std::string);
+
+		// Getters
+		std::string	getRawRequest(void);
+		std::string	getMethod(void);
+		std::string	getPath(void);
+		std::string	getProtocol(void);
+		std::string	getHost(void);
+
+	private:
+		std::string	_rawRequest;
+		std::string	_method;
+		std::string	_path;
+		std::string	_protocol;
+		std::string	_host;
+};
+
+#endif
