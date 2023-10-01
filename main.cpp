15404c6bea40b5a986c31fff1edb6d3378ae7d9d
Fernando Manzanares Montes <fmanzana@student.42malaga.com>
2023-10-01 17:14:42 +0200

    Server destructor and main modified and html files added

--- main.cpp
+++ main.cpp
@@ -16,40 +16,47 @@ void leaks(void) {
 int main(void) {
 	atexit(leaks);
 
-	std::vector<std::string> methods;
-	methods.push_back("GET");
-	std::vector<Route *> routes;
-	routes.push_back(new Route(methods, "./server1", "/", false, "index.html"));
-	methods.clear();
-	methods.push_back("POST");
-	methods.push_back("DELETE");
-	routes.push_back(new Route(methods, "./server1/storage", "/storage", true, "none"));
+	WebServs *c1 = new WebServs();
+	try {
+		std::vector<std::string> methods;
+		methods.push_back("GET");
+		std::vector<Route *> routes;
+		routes.push_back(new Route(methods, "./server1", "/", false, "index.html"));
+		methods.clear();
+		methods.push_back("POST");
+		methods.push_back("DELETE");
+		routes.push_back(new Route(methods, "./server1/storage", "/storage", true, "none"));
 
-	std::vector<int> port;
-	port.push_back(8080);
-	port.push_back(8081);
+		std::vector<int> port;
+		port.push_back(8080);
+		port.push_back(8081);
 
-	WebServs c1;
-	Server *s1 = new Server(port, routes, "127.0.0.1");
-	c1.addServer(s1);
+		Server *s1 = new Server(port, routes, "10.13.2.6");
+		c1->addServer(s1);
 
-	routes.clear();
-	methods.clear();
-	methods.push_back("GET");
-	routes.push_back(new Route(methods, "./server2", "/", true, "hello.html"));
-	methods.clear();
-	methods.push_back("GET");
-	routes.push_back(new Route(methods, "./server2/images", "/images", true, "default.html"));
+		routes.clear();
+		methods.clear();
+		methods.push_back("GET");
+		routes.push_back(new Route(methods, "./server2", "/", true, "hello.html"));
+		methods.clear();
+		methods.push_back("GET");
+		routes.push_back(new Route(methods, "./server2/images", "/images", true, "default.html"));
 
-	port.clear();
-	port.push_back(8001);
-	port.push_back(8002);
+		port.clear();
+		port.push_back(8001);
+		port.push_back(8002);
 
-	Server *s2 = new Server(port, routes, "0.0.0.0");
-	c1.addServer(s2);
+		Server *s2 = new Server(port, routes, "127.0.0.1");
+		c1->addServer(s2);
 
-	signal(SIGINT, signalHandler);
-	c1.runWebServs();
+		signal(SIGINT, signalHandler);
+		signal(SIGQUIT, signalHandler);
+		c1->runWebServs();
+		delete c1;
+	} catch(std::exception e) {
+		std::cout << e.what() << std::endl;
+		delete c1;
+	}
 
 	return (0);
 }
